18647cf1885940e0554e6c3e5aadd1c7b80f72ff
mudrap2706 <pandya.mu@northeastern.edu>
2024-10-15 23:22:51 -0400

    Setup table layout for AdminPage

--- src/View/AdminPage.java
+++ src/View/AdminPage.java
@@ -1,3 +1,4 @@
+//Set up basic table layout for displaying tasks.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
