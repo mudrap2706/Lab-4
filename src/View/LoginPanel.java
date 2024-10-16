e3b3c4272ab6a66650b9f7e18c59355635fa2c4f
mudrap2706 <pandya.mu@northeastern.edu>
2024-10-15 23:23:42 -0400

    Add login fields and button

--- src/View/LoginPanel.java
+++ src/View/LoginPanel.java
@@ -1,3 +1,4 @@
+//Add text fields and a button for login.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
