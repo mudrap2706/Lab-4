d25cd59a853e84d6f7f9f30297e28faef4220cb4
mudrap2706 <pandya.mu@northeastern.edu>
2024-10-15 23:28:01 -0400

    Implement navigation with CardLayout

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -1,3 +1,4 @@
+//Implement navigation between different views using CardLayout.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
