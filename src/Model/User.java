16bf32cef53637e1f0d3a757ba3d80f807237196
mudrap2706 <pandya.mu@northeastern.edu>
2024-10-15 22:56:59 -0400

    Add user attributes

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,3 +1,4 @@
+//Add attributes like username, password, and role.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
