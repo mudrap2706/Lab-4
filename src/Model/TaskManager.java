8c5bac28b6e95f41d434be6692b69626c45c64d7
mudrap2706 <pandya.mu@northeastern.edu>
2024-10-15 22:56:00 -0400

    Add method to store tasks

--- src/Model/TaskManager.java
+++ src/Model/TaskManager.java
@@ -1,3 +1,4 @@
+//Implement a method to add tasks to a list.
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
